d5e45aa73ff5cc502217fc0bfd451d54feba0713
fank <fankcoder@gmail.com>
2022-12-05 13:33:12 +0800

    update

--- dw/dw.py
+++ dw/dw.py
@@ -3,6 +3,7 @@ import json
 import os
 import shutil
 import time
+import asyncio
 from io import BytesIO
 
 headers = {
@@ -15,22 +16,44 @@ f = open('./data.json')
 data = json.load(f)
 f.close()
 
+Proxy = False #开启代理Ture/False
+
 def request_download(tag, name, url):
-    import requests
-    r = requests.get(url, headers=headers, stream=True)
-    _file = './{}/{}.jpg'.format(tag, name)
+    try:
+        if Proxy:
+            proxies = {
+                'http': 'http://121.13.252.60:41564',
+                'https': 'http://121.13.252.60:41564',
+            }
 
-    with open(_file, 'wb') as out_file:
-        shutil.copyfileobj(BytesIO(r.content), out_file)
+            r = requests.get(url, headers=headers, stream=True, timeout=10, proxies=proxies)
+        else:
+            r = requests.get(url, headers=headers, stream=True, timeout=10)
+        _file = './{}/{}.jpg'.format(tag, name)
 
-for item in data.get("Book"):
-    tag = item.get("tag")
-    folder = os.path.exists(tag)
-    if not folder:
-        os.makedirs(tag)     
+        with open(_file, 'wb') as out_file:
+            shutil.copyfileobj(BytesIO(r.content), out_file)
+    except:
+        return
 
-    imgs = item.get("imgs")
-    for i in imgs:
-        print(tag, i.get("Name"), i.get("Url"))
-        request_download(tag, i.get("Name"), i.get("Url"))
-        time.sleep(3)
+async def main():
+    num = 0
+    for item in data.get("Book"):
+        tag = item.get("tag")
+        folder = os.path.exists(tag)
+        if not folder:
+            os.makedirs(tag)     
+
+        imgs = item.get("imgs")
+        for i in imgs:
+            # print(tag, i.get("Name"), i.get("Url"))
+            print('正在下载第{}个...'.format(num))
+            num += 1
+            request_download(tag, i.get("Name"), i.get("Url"))
+
+if __name__ == "__main__":
+    coroutine = main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(coroutine) # 将协程对象添加到事件循环，运行直到结束
+    print(os.getpid())
+    loop.close() # 关闭事件循环
